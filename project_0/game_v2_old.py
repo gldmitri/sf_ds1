31ebb22f30667f026f495ca0b9d0d182c6bc1508
gldmitri <gldmitri@gmail.com>
2022-12-29 18:40:26 +0300

    новый алгоритм

--- project_0/game_v2_old.py
+++ project_0/game_v2_old.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def new_predict(number:int=1) -> int:
-    """Угадываем число посредством сжатия интервала в 2 раза после каждой попытки
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -12,34 +12,20 @@ def new_predict(number:int=1) -> int:
 
     count = 0
 
-    b1 = 1
-    b2 = 101
-    
-    predict_number = b1 + (b2 - b1) // 2
-
     while True:
         count += 1
-        # predict_number = np.random.randint(1, 101) # предполагаемое число
-        
+        predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
-        elif number < predict_number:
-            b2 = predict_number
-        else:
-            b1 = predict_number
-        
-        predict_number = b1 + (b2 - b1) // 2
-    
-    #print('Число:', number, '  Кол-во попыток:', count)
     return(count)
 
 #print(f'Количество попыток: {random_predict()}')
 
-def score_game(f_predict) -> int:
+def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
     Args:
-        f_predict ([type]): функция угадывания
+        random_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
@@ -47,10 +33,10 @@ def score_game(f_predict) -> int:
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # 1000 !!! загадали список чисел
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
 
     for number in random_array:
-        count_ls.append(f_predict(number))
+        count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
@@ -59,5 +45,5 @@ def score_game(f_predict) -> int:
 
 # RUN
 if __name__ == '__main__':
-    score_game(new_predict)
+    score_game(random_predict)
     
